85e2ca010b5caa4c752395a9c335cfba93cece10
Shivansh Anand <shivanshanandisminame@outlook.com>
2020-04-07 20:21:49 +0530

    added loading animatios

--- YATicTacToe.cpp
+++ YATicTacToe.cpp
@@ -4,19 +4,25 @@
  */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 char board[3][3];
 int hplmx; // human player last move x
 int hplmy; // human player last move y
+int choice;
 
 void initBoard(void);
 void displayBoard(int);
 void getPlayer1Move(void);
 void getPlayer2Move(void);
-int getCompMove(void);
+void getCompMove(void);
+int yenniferAmb(void);
+void printCenteredMenu(char[], int, int);
+void delay(int);
 void showWinScreen(int);
 bool calcWin(int);
+void menuScreen(void);
 
 void initBoard()
 {
@@ -51,12 +57,22 @@ void displayBoard(int playerWhichMovedLast)
 
 	if (playerWhichMovedLast == 1)
 	{
-		getCompMove();
-		displayBoard(2);
+		if (choice == 1)
+		{
+			getPlayer2Move();
+		}
+		else
+		{
+			getCompMove();
+		}
 	}
-	else
+	else if(playerWhichMovedLast == 2)
 	{
 		getPlayer1Move();
+	}
+	else if (playerWhichMovedLast == 0)
+	{
+		cout << "Game Over !";
 	}
 }
 
@@ -142,33 +158,93 @@ void showWinScreen(int whichPlayerWon)
 		//system("cls");
 		for (double j = 0; j < 1000000; j += 0.01);
 	}
-	exit(0);
+
+	menuScreen();
+}
+
+void delay(int length)
+{
+	double step = 0.04;
+	double i;
+	for (i = 0; i < length; i += step);
+}
+
+void printCenteredMenu(char msg[], int baseLength, int t)
+{
+	if (t == 1)
+		cout << "\t";
+
+	int n = (int)((baseLength - strlen(msg)) / 2);
+	int i;
+
+	for (i = 0; i < n; i++)
+		cout << " ";
+
+	cout << msg << "\n";
 }
 
 void menuScreen()
 {
-	cout << "\t***  ***   ***     ********     ********     ********  \n";
-	cout << "\t **  **   ** **   **********   **********   ********** \n";
-	cout << "\t  ****   *******      **           **           **     \n";
-	cout << "\t  **    **     **     **           **           **     \n";
-	cout << "\t **    **       **    **           **           **     \n";
+	char msgs[][100] = {
+		"Yet Another Tic Tac Toe Game",
+		"made by Shivansh Anand",
+		"1. Human VS. Human",
+		"2. Human VS. Computer",
+		"3. Exit"
+	};
+	char mainLogo[][100] = {
+		"***  ***   ***     ********     ********     ********",
+		" **  **   ** **   **********   **********   **********",
+		"  ****   *******      **           **           **",
+		"  **    **     **     **           **           **",
+		" **    **       **    **           **           **"
+	};
 
-	cout << "\t\tYet Another Tic Tac Toe Game\n\t\tmade by Shivansh Anand\n\n\t\t1. Human VS. Human\n\t\t2. Human VS. Computer\n\t\t3. Any other integer to exit";
-	int choice;
+	int len = strlen(mainLogo[4]);
+	int i;
+
+	system("cls");
+	for (i = 0; i < 5; i++)
+	{
+		cout << "\t" << mainLogo[i] << "\n";
+		delay(2000000);
+	}
+
+	int spLen =  (int)((len - strlen(msgs[0])) / 2);
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (i == 0 || i == 2)
+			cout << "\n";
+		printCenteredMenu(msgs[i], len, 1);
+		delay(5000000);
+	}
+	
 	cin >> choice;
-	if (choice > 0)
+	if (choice == 3)
+	{
+		exit(0);
+	}
+	else if (choice == 1 || choice == 2)
 	{
 		initBoard();
 		displayBoard(2);
 	}
 	else
 	{
+		cout << "Invalid choice";
 		exit(0);
 	}
 }
 
+void getCompMove()
+{
+	yenniferAmb();
+	displayBoard(2);
+}
 
-int getCompMove()
+
+int yenniferAmb()
 {
 	int i;
 	int count = 0;
@@ -294,8 +370,6 @@ int getCompMove()
 
 int main()
 {
-	//menuScreen();
-	initBoard();
-	displayBoard(2);
+	menuScreen();
 	return 0;
 }
